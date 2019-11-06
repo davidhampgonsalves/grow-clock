08e8f65a63b510382d216d2a27d274ef854076dd
David Hamp-Gonsalves <davidhampgonsalves@gmail.com>
2019-11-06 12:00:32 -0400

    switch to ESP8266

--- README.md
+++ README.md
@@ -1,21 +1,17 @@
 # Grow Clock
-[Arduino](https://store.arduino.cc/usa/arduino-mkr1000) powered, recyclable grow clock for children.
+ES8266 powered, recyclable grow clock for children.
 
 <p style="text-align:center">
   <img src="https://github.com/davidhampgonsalves/grow-clock/blob/master/grow-clock.gif" width="100%">
 </p>
 
-My version was made to be recyclable, since these devices are only useful while children learn when to wake. I used an [Arduino MKR1000](https://store.arduino.cc/usa/arduino-mkr1000) (because I had a spare one) but any Arduino compatible board with a RTC(real time clock) will work. The MKR1000 is well suited to this task due to its as low power consumption in standby.
-
-## Note
-Once the MKR1000 goes to sleep the USB port will be disabled and Serial communication will be disrupted. Hit the reset button twice to restore the device back to original settings.
+Made out of reused cardboard since these devices are only useful while children learn when to wake. I used an [Arduino originally](https://github.com/davidhampgonsalves/grow-clock/commit/130541bbc1a834789b63a736866fe7e3ff2ab5e8) but switched to an ESP8266 because the clock was still being used and wanted to use NTP time via wifi.
 
 # Install
 I use [platform.io cli](https://platformio.org/get-started/cli) but the Arduino IDE will work just as well.
 
-Connect a servo to ground / Vcc and the signal wire to pin 6.
+Connect a servo to ground / Vcc and the signal wire to pin D4.
 ```
 pio devices list
-pio run --target upload
-pio device monitor --port /dev/cu.usbmodem1421
+pio run
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,31 @@
 #include <Arduino.h>
 #include <Servo.h>
-#include <time.h>
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <NTPClient.h>
+#include <TimeLib.h>
 #include <WiFiUdp.h>
 
 Servo servo;
 
-
-const char * ssid = "Volta";
-const char * pass = "V0lt@2018"; // I don't mind people using my wifi if they are in the area
+const char * ssid = "BELL502";
+const char * pass = "2167ACD3E6AF"; // I don't mind people using my wifi if they are in the area
 
 const long utcOffsetInSeconds = 3600;
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
-const long MAX_SLEEP_DURATION_IN_MINUTES = 70;
-const int SERVO_PIN = 6;
-
-int DAY_HOUR_START = 6;
-int DAY_MIN_START = 50;
-
-int DAY_HOUR_END = 19;
-int DAY_MIN_END = 0;
+const unsigned long MAX_SLEEP_DURATION = 70 * 60;
+const int SERVO_PIN = D4;
 
 int MOVE_DELAY = 90;
+
+const unsigned long DAY_START = ((6 * 60) + 50) * 60;
+const unsigned long DAY_END = 19 * 60 * 60;
+
+IPAddress timeServerIP;
+const char* ntpServerName = "time.nist.gov";
+const int NTP_PACKET_SIZE = 48;
+byte packetBuffer[NTP_PACKET_SIZE];
+WiFiUDP udp;
+unsigned int localPort = 2390;
 
 typedef struct {
   unsigned long lastTime;
@@ -33,31 +33,98 @@ typedef struct {
 } stateStruct __attribute__((aligned(4)));
 stateStruct state;
 
-int toMinutes(int hours, int minutes) {
-  return (hours * 60) + minutes;
+unsigned long toSecondsInDay(unsigned long t) {
+  return (hour(t) * 60 * 60) + (minute(t) * 60) + second(t);
 }
 
-bool isDayTime() {
-  int minutes = toMinutes(hour(), minute());
-  return minutes >= toMinutes(DAY_HOUR_START, DAY_MIN_START) && minutes < toMinutes(DAY_HOUR_END, DAY_MIN_END);
+bool isDayTime(unsigned long c) {
+  bool isDayTime = c >= DAY_START && c < DAY_END;
+
+  Serial.print("is day time? ");
+  Serial.print(isDayTime);
+  Serial.print(" @ ");
+  Serial.print(c);
+  Serial.println(" seconds.");
+
+  return isDayTime;
 }
 
 void moveTo(int from, int to) {
   servo.attach(SERVO_PIN);
-  bool isForward = to < from;
-
-  for (int pos = from ; pos != to ; pos += isForward ? -1 : 1) {
-    servo.write(pos);
-    delay(MOVE_DELAY);
-  }
+  servo.write(to);
+  delay(1000);
   servo.detach();
 }
 
-void updateClock() {
-  if(isDayTime()) {
-    moveTo(110, 30);
+// from sketch examples
+void sendNTPpacket(IPAddress& address) {
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  packetBuffer[0] = 0b11100011;
+  packetBuffer[1] = 0;
+  packetBuffer[2] = 6;
+  packetBuffer[3] = 0xEC;
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  udp.beginPacket(address, 123);
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
+}
+
+// from sketch examples
+unsigned long fetchNTPTime() {
+  Serial.print("syncing time");
+  udp.begin(localPort);
+  WiFi.hostByName(ntpServerName, timeServerIP);
+  sendNTPpacket(timeServerIP);
+  delay(200);
+
+  int cb;
+  int counter = 0;
+  while(!(cb = udp.parsePacket())) {
+    Serial.print(".");
+    delay(100);
+    counter ++;
+    if(counter > 50) {
+      Serial.println("failed.");
+      return fetchNTPTime();
+    }
+  }
+  Serial.println("done.");
+
+  udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+  unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+  unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+  unsigned long secsSince1900 = highWord << 16 | lowWord;
+  const unsigned long seventyYears = 2208988800UL;
+  unsigned long epoch = secsSince1900 - seventyYears;
+
+  return epoch;
+}
+
+void setupSystemTime() {
+  if(ESP.getResetInfoPtr()->reason != 5) {
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
+    Serial.print("connecting to wifi");
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(50);
+    }
+    Serial.println("done.");
+
+    const unsigned long currentTime = fetchNTPTime();
+    setTime(currentTime);
+    adjustTime(60 * 60 * -4); // apply TZ (but not DST)
   } else {
-    moveTo(30, 110);
+    Serial.print("using time from RTC mem: ");
+    Serial.print(state.lastTime);
+    Serial.print(", ");
+    Serial.println(state.sleepDurationInSeconds);
+
+    setTime(state.lastTime + state.sleepDurationInSeconds); // this is inacurate and so is the "rtc" of the ESP but close enough
   }
 }
 
@@ -66,46 +133,46 @@ void setup() {
   delay(200);
 
   system_rtc_mem_read(64, &state, sizeof(state));
-  const unsigned long currentTimeInSeconds = state.lastTime + state.sleepDurationInSeconds;
 
-  if(ESP.getResetInfoPtr()->reason != 5) {
-    Serial.print("connecting");
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, pass);
-    while (WiFi.status() != WL_CONNECTED) { Serial.print("."); delay(100); }
-    Serial.print("done\n");
-    WiFi.disconnect();
-    WiFi.mode(WIFI_OFF);
-    WiFi.forceSleepBegin();
+  setupSystemTime();
 
-    Serial.print("\nrequesting time... ");
-    timeClient.begin();
-    timeClient.update();
-    // TODO: do I need to set time or does the time client do that? setTime();
-    timeClient.end();
-    Serial.print("done\n");
-  } else {
-    setTime(currentTimeInSeconds); // this is inacurate and so is the "rtc" of the ESP but close enough
+  // if there was a day / night change between last run and now then update clock position
+  unsigned long current = toSecondsInDay(now());
+  if(isDayTime(toSecondsInDay(state.lastTime)) != isDayTime(current)) {
+    if(isDayTime(current)) {
+      Serial.println("its day time.");
+      moveTo(110, 30);
+    } else {
+      Serial.println("its night time.");
+      moveTo(30, 110);
+    }
   }
 
-  updateClock();
-
-  const int currentMinutes = toMinutes(hour(), minute());
-  const int minutesTillNight = toMinutes(DAY_HOUR_END, DAY_MIN_END) - currentMinutes;
-  const int minutesTillMorning = toMinutes(DAY_HOUR_START, DAY_MIN_START) - currentMinutes;
-  const int minutesToSleep = minutesTillNight < minutesTillMorning ? minutesTillNight : minutesTillMorning;
-
-  if(minutesToSleep < MAX_SLEEP_DURATION_IN_MINUTES)
-    state.sleepDurationInSeconds = minutesToSleep * 1000 * 1000;
+  unsigned long secToSleep;
+  if(current >= DAY_END)
+    secToSleep = MAX_SLEEP_DURATION;
+  else if(current >= DAY_START)
+    secToSleep = DAY_END - current;
   else
-    state.sleepDurationInSeconds = MAX_SLEEP_DURATION_IN_MINUTES * 60;
+    secToSleep = DAY_START - current;
+
+  state.sleepDurationInSeconds = secToSleep < MAX_SLEEP_DURATION ? secToSleep : MAX_SLEEP_DURATION;
+
+  Serial.print("sleeping for ");
+  Serial.print(state.sleepDurationInSeconds);
+  Serial.print(" / ");
+  Serial.print(MAX_SLEEP_DURATION);
+  Serial.println(" seconds.");
 
   state.lastTime = now();
+
+  Serial.print("writing in time to RTC: ");
+    Serial.print(state.lastTime);
+    Serial.print(", ");
+    Serial.println(state.sleepDurationInSeconds);
+
   system_rtc_mem_write(64, &state, sizeof(state));
   ESP.deepSleep(state.sleepDurationInSeconds * 1000 * 1000);
 }
 
-//https://github.com/dalmatianrex/articles/blob/master/makerpro-esp8266-ntp/ESP8266_Time.ino
-//
-//https://github.com/esp8266/Arduino/blob/master/libraries/esp8266/examples/NTP-TZ-DST/NTP-TZ-DST.ino
 void loop() { }
